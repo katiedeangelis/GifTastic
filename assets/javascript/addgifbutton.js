0eeb0f072bbd3838d9da8849cc63c2f0b696f3b1
Katie DeAngelis <katie@Katies-MacBook-Pro.local>
2018-01-01 19:18:35 -0500

    Function to render new buttons after search.

--- assets/javascript/addgifbutton.js
+++ assets/javascript/addgifbutton.js
@@ -1,6 +1,30 @@
 // Initial array of show buttons
 var showButtons = ["Rick and Morty", "Futurama", "Bob's Burger", "Archer"];
 
+// Function for displaying show buttons
+function renderButtons() {
+
+    // Delete the show buttons prior to adding a new one (So there are no duplicates)
+    $("#buttons-view").empty();
+
+    // Looping through the array of shows to create buttons
+    for (var i = 0; i < showButtons.length; i++) {
+
+        // Then dynamicaly generating buttons for each show in the array
+        var a = $("<button>");
+
+        a.attr("type", "button");
+        // Adding a class of btn btn-success to our button for bootstrap buttons
+        a.addClass("btn btn-success");
+        // Adding a data-attribute
+        a.attr("data-name", showButtons[i]);
+        // Providing the initial button text
+        a.text(showButtons[i]);
+        // Adding the button to the buttons-view div
+        $("#buttons-view").append(a);
+    }
+}
+
 $("#add-gif-button").on("click", function (event) {
     event.preventDefault();
 
@@ -12,8 +36,8 @@ $("#add-gif-button").on("click", function (event) {
     // Adding movie from the textbox to our array
     showButtons.push(newShow);
 
-    console.log (showButtons);
+    console.log(showButtons);
 
-    // // Calling renderButtons which handles the processing of our movie array
-    // renderButtons();
+    // Calling renderButtons which handles the processing of the showButtons array
+    renderButtons();
 });
